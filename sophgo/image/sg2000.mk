f0a53ca590ed5e39ee9aec318822c897c635b6af
draftbottle <2641473760@qq.com>
2024-08-20 13:26:59 +0800

    change 1

--- sophgo/image/sg2000.mk
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-only
-define Device/milk-v-duos
-  DEVICE_VENDOR := milkv.io
-  DEVICE_MODEL := milkv-duos
-  DEVICE_DTS_DIR := ${PWD}/sg2000/dts
-  DEVICE_DTS := sg2000_milkv_duos_glibc_arm64_sd
-endef
-TARGET_DEVICES += milk-v-duos
