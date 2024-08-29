cfab52dbf78975283133f71e6c69619dd663fe1d
draftbottle <2641473760@qq.com>
2024-08-29 21:31:37 +0800

    fix: fix the u-boot compile error.

--- sophgo/cv1813h/patch-milkv-duo.sh
+++ sophgo/cv1813h/patch-milkv-duo.sh
@@ -1,11 +1,18 @@
-cp ./dt-bindings/cv181x-clock.h  /home/liushijiao/openwrt/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/clock/
-cp ./dt-bindings/cv181x-dmamap.h  /home/liushijiao/openwrt/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/dma/
-cp ./dt-bindings/cv181x-resets.h  /home/liushijiao/openwrt/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/reset/
+cp ./dt-bindings/cv181x-clock.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/clock/
+cp ./dt-bindings/cv181x-dmamap.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/dma/
+cp ./dt-bindings/cv181x-resets.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/reset/
 
-cp ./dt-bindings/cv181x-clock.h  /home/liushijiao/openwrt/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/clock/
-cp ./dt-bindings/cv181x-dmamap.h  /home/liushijiao/openwrt/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/dma/
-cp ./dt-bindings/cv181x-resets.h  /home/liushijiao/openwrt/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/reset/
+cp ./dt-bindings/cv181x-clock.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/clock/
+cp ./dt-bindings/cv181x-dmamap.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/dma/
+cp ./dt-bindings/cv181x-resets.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.161/include/dt-bindings/reset/
 
+cp ./dt-bindings/cv181x-clock.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.125/include/dt-bindings/clock/
+cp ./dt-bindings/cv181x-dmamap.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.125/include/dt-bindings/dma/
+cp ./dt-bindings/cv181x-resets.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_glibc/linux-sophgo_cv1813h/linux-5.15.125/include/dt-bindings/reset/
+
+cp ./dt-bindings/cv181x-clock.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.125/include/dt-bindings/clock/
+cp ./dt-bindings/cv181x-dmamap.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.125/include/dt-bindings/dma/
+cp ./dt-bindings/cv181x-resets.h  /home/liushijiao/istoreos/build_dir/target-aarch64_generic_musl/linux-sophgo_cv1813h/linux-5.15.125/include/dt-bindings/reset/
 
 echo "patch finish."
 
